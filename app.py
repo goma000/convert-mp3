dc48a1f7cfa7db261f806326dcda900d652bb12e
goma000 <121588115+goma000@users.noreply.github.com>
2024-12-12 15:19:41 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,33 @@
-import streamlit as st
 from moviepy.video.io.VideoFileClip import VideoFileClip
+import streamlit as st
 import os
 
-st.title("MP4 to MP3 Converter")
+# タイトル表示
+st.title("MP4/MOV to MP3 Converter")
 
-# ファイルアップロード
-uploaded_file = st.file_uploader("Upload MP4 File", type=["mp4"])
+# ファイルアップロードセクション
+uploaded_file = st.file_uploader("Upload MP4 or MOV File", type=["mp4", "mov"])
 
+# ファイルがアップロードされたら処理を実行
 if uploaded_file is not None:
-    # 一時ファイルに保存
-    temp_file = "temp_video.mp4"
+    # 一時ファイルとして保存
+    temp_file = "temp_video." + uploaded_file.name.split(".")[-1]
     with open(temp_file, "wb") as f:
         f.write(uploaded_file.getbuffer())
-    st.success("File Uploaded Successfully!")
+    st.success(f"'{uploaded_file.name} をアップロードしました!")
     
-    # 変換処理
+    # MP3への変換
     try:
-        st.info("Converting to MP3...")
+        st.info("MP3に変換中...")
         output_file = "output_audio.mp3"
         video = VideoFileClip(temp_file)
         video.audio.write_audiofile(output_file, codec='libmp3lame')
         
         # ダウンロードリンク表示
         with open(output_file, "rb") as f:
-            st.download_button(label="Download MP3", data=f, file_name="output_audio.mp3", mime="audio/mp3")
+            st.download_button(label="Download MP3 File", data=f, file_name="output_audio.mp3", mime="audio/mp3")
         
-        st.success("Conversion Successful!")
+        st.success("変換に成功しました!ダウンロードボタンからmp3ファイルをダウンロードしてください")
     except Exception as e:
         st.error(f"An error occurred: {e}")
     finally:
